984798fe0dbf06c5f002b7490783e0bf3de28c01
Wilmar Florez <wflorez52@gmail.com>
2020-09-27 22:19:01 -0500

    Added firebase config

---
+++ js/auth/autenticacion.js
@@ -0,0 +1,57 @@
+class Autenticacion {
+  autEmailPass(email, password) {
+    //$('#avatar').attr('src', 'imagenes/usuario_auth.png')
+    //Materialize.toast(`Bienvenido ${result.user.displayName}`, 5000)
+    //$('.modal').modal('close')
+
+  }
+
+  crearCuentaEmailPass(email, password, nombres) {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(result => {
+        result.user.updateProfile({
+          displayName: nombres
+        })
+
+        const config = {
+          url: 'http://localhost:3000/'
+        }
+
+        result.user
+          .sendEmailVerification(config)
+          .catch(error => {
+            console.error(error)
+            Materialize.toast(error.message, 4000)
+          })
+
+        firebase.auth().signOut()
+
+        Materialize.toast(
+          `Bienvenido ${nombres}, debes realizar el proceso de verifiación`,
+          4000
+        )
+
+        $('.modal').modal('close')
+      })
+      .catch(error => {
+        console.error(error)
+        Materialize.toast('ERROR', error.message, 4000)
+      })
+  }
+
+  authCuentaGoogle() {
+    //$('#avatar').attr('src', result.user.photoURL)
+    //$('.modal').modal('close')
+    //Materialize.toast(`Bienvenido ${result.user.displayName} !! `, 4000)
+  }
+
+  authCuentaFacebook() {
+    //$('#avatar').attr('src', result.user.photoURL)
+    //$('.modal').modal('close')
+    //Materialize.toast(`Bienvenido ${result.user.displayName} !! `, 4000)
+  }
+
+  authTwitter() {
+    // TODO: Crear auth con twitter
+  }
+}
---
+++ js/config/ConfigFirebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyC96zYDhWchLn1OGODChnl-UkzS9zGLHoI',
+  authDomain: 'blogeekwilmar.firebaseapp.com',
+  databaseURL: 'https://blogeekwilmar.firebaseio.com',
+  projectId: 'blogeekwilmar',
+  storageBucket: 'blogeekwilmar.appspot.com',
+  messagingSenderId: '1086417898540',
+  appId: '1:1086417898540:web:8d6f3badd5e0461251d589',
+  measurementId: 'G-MEBY3V1KW0',
+}
